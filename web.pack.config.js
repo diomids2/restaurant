9d3a16a2d74ea8c68351a62876608732a80feecb
Claudiomir <diomids2@hotmail.com>
2023-03-12 23:38:29 -0300

    ajuste pastas

---
+++ web.pack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+    test: /\.less$/,
+    use: [
+      'vue-style-loader',
+      'css-loader',
+      'less-loader']
+  }
+  ]
+}
+};
